78f7fc6aa549ce397177e13be8cc8514a8ed6dfd
Juna <juna.idler@gmail.com>
2021-08-29 20:30:00 +0900

    プロジェクト ファイルを追加します。

---
+++ LyricsSearcherPlugin/LyricsSearcher.cs
@@ -0,0 +1,21 @@
+﻿
+
+//アセンブリ名はユニークなものへ変更
+//
+//ここの名前空間名、クラス名、メソッド名は全てこのまま固定
+
+namespace Titalyver2
+{
+    public class LyricsSearcher
+    {
+        public string[] Search(string title,
+                               string[] artists,
+                               string album,
+                               string path,     //ファイルの場合ファイルのパス
+                               string param)    //SeachListに書かれたパラメータ
+        {
+            string testoutput = "Test Plugin String\n" + title + "\n" + artists[0] + "\n" + album + "\n" + path + "\n" + param;
+            return new string[] { testoutput,"multitest" };
+        }
+    }
+}
---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.IO;
+using System.Reflection;
+
+
+namespace Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private static readonly string dllpath = @"..\..\..\..\LyricsSearcherPlugin\bin\Debug\net5.0";
+
+        private dynamic Searcher;
+        private void Button_Click(object sender, RoutedEventArgs eve)
+        {
+            string[] dll = Directory.GetFiles(dllpath, "*.dll");
+
+            try
+            {
+                if (Searcher == null)
+                {
+                    Assembly assembly = Assembly.LoadFrom(dll[0]);
+                    Type type = assembly.GetType("Titalyver2.LyricsSearcher", true);
+                    Searcher = Activator.CreateInstance(type);
+                    if (Searcher == null)
+                        return;
+                }
+                string[] result = Searcher.Search(TextBoxTitle.Text, new string[] { TextBoxArtist.Text }, TextBoxAlbum.Text, TextBoxPath.Text, TextBoxParam.Text);
+                TextBoxResult.Text = string.Join("\n========== multi result separate ==========\n", result);
+            }
+            catch (Exception e)
+            {
+                TextBoxResult.Text = "例外が発生しました。\n" + e.Message;
+                return;
+            }
+        }
+    }
+}
