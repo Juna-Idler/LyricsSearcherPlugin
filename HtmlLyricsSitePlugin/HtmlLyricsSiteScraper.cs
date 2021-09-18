f2ac5ed3db7519a4dbe1c88c28ec2f62973cf24b
Juna <juna.idler@gmail.com>
2021-09-18 20:08:52 +0900

    とりあえず実用できなくはない程度にできたか

---
+++ HtmlLyricsSitePlugin/HtmlLyricsSiteScraper.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.IO;
+
+using System.Text.RegularExpressions;
+using System.Web;
+
+using System.Net.Http;
+
+
+namespace LyricsSearcherPlugin
+{
+    public class HtmlLyricsSiteScraper
+    {
+        public class SiteParameter
+        {
+            public string Name { get; set; }
+
+            public string Encoding { get; set; }
+
+            public Uri Url { get; set; }
+            public string ParamFormat { get; set; }
+
+            public string ListBlockRegex { get; set; }
+            public string ListItemRegex { get; set; }
+            public string ItemUrlRegex { get; set; }
+            public string ItemTitleRegex { get; set; }
+            public string ItemArtistRegex { get; set; }
+
+
+            public string LyricsBlockRegex { get; set; }
+
+            public struct Replacer
+            {
+                public string Regex { get; set; }
+                public string ReplaceText { get; set; }
+            }
+            public Replacer[] LyricsReplacers { get; set; }
+        }
+
+
+
+        public struct ListData
+        {
+            public Uri LyricsPageUrl { get; set; }
+            public string Title { get; set; }
+            public string Artist { get; set; }
+        }
+
+        private static readonly HttpClient client = new HttpClient();
+
+        static HtmlLyricsSiteScraper()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        public static ListData[] GetList(SiteParameter param, string title, string artist)
+        {
+            Encoding encoding = Encoding.GetEncoding(param.Encoding);
+
+            title = HttpUtility.UrlEncode(title, encoding);
+            artist = HttpUtility.UrlEncode(artist, encoding);
+
+            string url_param = param.ParamFormat.Replace("{title}", title).Replace("{artist}", artist);
+
+            HttpResponseMessage response;
+            try
+            {
+                response = client.GetAsync(param.Url + url_param).Result;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+
+            Encoding content_encoding = Encoding.GetEncoding(response.Content.Headers.ContentType.CharSet);
+            Stream stream = response.Content.ReadAsStream();
+            StreamReader sr = new(stream, content_encoding);
+            string content = sr.ReadToEnd();
+
+            List<ListData> lists = new();
+
+            Match block = Regex.Match(content, param.ListBlockRegex, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+            if (!block.Success)
+                return null;
+            var items = Regex.Matches(block.Groups[1].Value, param.ListItemRegex, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+            foreach (Match elm in items)
+            {
+                string item = elm.Groups[1].Value;
+                ListData list = new();
+                Match m = Regex.Match(item, param.ItemUrlRegex, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+                if (!m.Success)
+                    continue;
+                list.LyricsPageUrl = new Uri(param.Url, m.Groups[1].Value);
+                m = Regex.Match(item, param.ItemTitleRegex, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+                if (!m.Success)
+                    continue;
+                list.Title = m.Groups[1].Value.Trim();
+                m = Regex.Match(item, param.ItemArtistRegex, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+                if (!m.Success)
+                    continue;
+                list.Artist = m.Groups[1].Value.Trim();
+                lists.Add(list);
+            }
+            return lists.ToArray();
+        }
+
+        public static string GetLyrics(Uri lyrics_page_url, SiteParameter param)
+        {
+            HttpResponseMessage response;
+            try
+            {
+                response = client.GetAsync(lyrics_page_url).Result;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+            Encoding content_encoding = Encoding.GetEncoding(response.Content.Headers.ContentType.CharSet);
+            Stream stream = response.Content.ReadAsStream();
+            StreamReader sr = new(stream, content_encoding);
+            string content = sr.ReadToEnd();
+
+            Match block = Regex.Match(content,param.LyricsBlockRegex, RegexOptions.Singleline | RegexOptions.IgnoreCase);
+            if (block == null)
+                return null;
+            string lyrics = block.Groups[1].Value;
+            if (param.LyricsReplacers != null)
+            {
+                foreach (var r in param.LyricsReplacers)
+                {
+                    lyrics = Regex.Replace(lyrics, r.Regex, r.ReplaceText, RegexOptions.Singleline);
+                }
+            }
+
+            return HttpUtility.HtmlDecode(lyrics);
+
+        }
+
+
+
+    }
+}
---
+++ HtmlLyricsSitePlugin/LyricsSearcher.cs
@@ -0,0 +1,66 @@
+﻿
+using System.IO;
+
+using System.Text.Json;
+using System.Diagnostics;
+
+using System.Web;
+
+
+using LyricsSearcherPlugin;
+//アセンブリ名はユニークなものへ変更
+//
+//ここの名前空間名、クラス名、メソッド名は全てこのまま固定
+
+namespace Titalyver2
+{
+    public class LyricsSearcher
+    {
+        private string JsonPath;
+        private HtmlLyricsSiteScraper.SiteParameter[] SiteList;
+        public string[] Search(string title,
+                               string[] artists,
+                               string album,
+                               string path,     //ファイルの場合ファイルのパス
+                               string param)    //SeachListに書かれたパラメータ
+        {
+            if (JsonPath == null)
+            {
+                string assemblyPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                string directory = Path.GetDirectoryName(assemblyPath);
+                JsonPath = Path.Combine(directory, "HtmlLyricsSitePluginSettings.json");
+            }
+
+            if (SiteList == null)
+            {
+                using FileStream fs = File.OpenRead(JsonPath);
+                var task = JsonSerializer.DeserializeAsync<HtmlLyricsSiteScraper.SiteParameter[]>(fs);
+                SiteList = task.AsTask().Result;
+            }
+
+            foreach (var site in SiteList)
+            {
+                var list = HtmlLyricsSiteScraper.GetList(site, title, artists[0]);
+                if (list != null)
+                {
+                    foreach (var item in list)
+                    {
+                        string lyrics = HtmlLyricsSiteScraper.GetLyrics(item.LyricsPageUrl, site);
+                        if (lyrics != null)
+                        {
+                            ProcessStartInfo pi = new()
+                            {
+                                FileName = item.LyricsPageUrl.AbsoluteUri,
+                                UseShellExecute = true,
+                            };
+                            Process.Start(pi);
+                            lyrics += $"\n\n{site.Name}\n{HttpUtility.HtmlDecode(item.Title)}\n{HttpUtility.HtmlDecode(item.Artist)}\n{item.LyricsPageUrl}";
+                            return new string[] { lyrics };
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
